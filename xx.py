bbd311525eab93a4033329b88d14b750f79700e8
kanchecxm <xianming.chu@kanche.com>
2017-09-26 16:22:42 +0800

    优化py，添加帮助，添版本号显示，处理更新方法

--- xx.py
+++ xx.py
@@ -48,12 +48,34 @@ def dockerExec(name = " "):
 	print("登入 %s bash：" % dockername)
 	os.system("docker exec -it  %s bash" % dockername)
 	return
+def helps( version ):
+        print """
+            xx工具（%s）使用帮助
+        了解更多请访问 https://github.com/iuv/xx
+        docker 相关操作
+          1.简化查询docker 命令，使用
+            xx dl [dockername]
+            查询容器输出日志，dockername 支持模糊搜索
+          2.简化进入docker bash 命令，使用
+            xx de [dockername]
+            登入容器bash，dockername 支持模糊搜索
+        """ % version
+        return
+def versions( version ):
+    print("xx v%s" % version);
 
 #主方法执行
+version = "0.1" # 版本号
 a1 = sys.argv[1]
-a2 = sys.argv[2]
+a2 = None
 a3 = None
+if(len(sys.argv) == 3):
+	a3 = sys.argv[2]
 if(len(sys.argv) == 4):
 	a3 = sys.argv[3]
 if(str(a1).startswith("d")):
 	docker(a1,a2,a3)
+elif a1 == "-h" or a1 == "help":
+	helps(version)
+elif a1 == "-v" or a1 == "version":
+	versions(version)
